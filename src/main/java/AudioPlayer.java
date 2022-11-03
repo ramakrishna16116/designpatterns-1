fc81b9d0c9f86885bc4d845b7ce749fd19754f94
jared-heidt <jared.heidt14@gmail.com>
2022-11-03 15:54:52 -0400

    added vlc handling to AudioPlayer.java

--- src/main/java/AudioPlayer.java
+++ src/main/java/AudioPlayer.java
@@ -1,10 +1,15 @@
 public class AudioPlayer implements MusicPlayer {
+    MediaAdapter mediaAdapter;
+    
     @Override
     public void playMusic(String audioType, String fileName) {
-        if(audioType.equalsIgnoreCase("mp3")) {
+        if (audioType.equalsIgnoreCase("mp3")) {
             System.out.println("Playing mp3 file: " + fileName);
+        } else if (audioType.equalsIgnoreCase("vlc")) {
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.playMusic(audioType, fileName);
         } else {
-            System.out.println("Audio type not supported for the file: " + fileName);
+            System.out.println("Media file not supported: " + audioType);
         }
     }
 }
